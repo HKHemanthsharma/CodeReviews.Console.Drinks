a9724af0ece62ce659dcbb4e1eec9e6268d5237e
HKHemanthsharma <hkhemanthsharma@gmail.com>
2025-04-01 02:36:44 +0530

    successfully retrieved drinks category list json response for the menu

---
+++ HKHemanthsharma.Drinks/HKHemanthsharma.Drinks/Cocktails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKHemanthsharma.Drinks
+{
+    public class Cocktails
+    {
+        private readonly string baseUrl;
+        public Cocktails(string baseUrl)
+        {
+            this.baseUrl = baseUrl;
+        }
+        public async Task<string> GetCategriesAsync()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                string categoriesUrl = baseUrl + "/list.php?c=list";
+                return await client.GetStringAsync(categoriesUrl);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return "empty";
+            
+        }
+    }
+}
---
+++ HKHemanthsharma.Drinks/HKHemanthsharma.Drinks/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HKHemanthsharma.Drinks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = "https://www.thecocktaildb.com/api/json/v1/1";
+            Cocktails cocktails = new Cocktails(baseUrl);
+            string result = cocktails.GetCategriesAsync().GetAwaiter().GetResult();
+            Console.WriteLine(result);
+        }
+    }
+}

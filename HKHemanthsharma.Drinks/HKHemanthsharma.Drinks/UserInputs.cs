3b3bfdf074c879e9c47e75b80c36e85e27cb9f24
HKHemanthsharma <hkhemanthsharma@gmail.com>
2025-04-02 23:59:06 +0530

    Project Finished

--- HKHemanthsharma.Drinks/HKHemanthsharma.Drinks/UserInputs.cs
+++ HKHemanthsharma.Drinks/HKHemanthsharma.Drinks/UserInputs.cs
@@ -8,7 +8,7 @@ namespace HKHemanthsharma.Drinks
         {
             var chosenCategory = AnsiConsole.Prompt(
                 new SelectionPrompt<string>()
-                    .Title("[pink3]Please Select a Category of drink you want to Order[/]")
+                    .Title("[pink3]Please Select a Category of drink that you want to Order[/]")
                     .PageSize(10)
                     .MoreChoicesText("[grey](Move up and down to reveal more categories)[/]")
                     .AddChoices(categories.drinkCategories.Select(d => d.CategoryName)));
